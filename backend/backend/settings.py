dcb92a894268142b94616d045f4e490772592415
EvgenyRomashov <rominnebarin@yandex.ru>
2023-06-19 14:29:41 +0300

    allowed_hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,12 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'supertasker.hopto.org',
+    '130.193.41.106',
+]
 
 
 # Application definition
